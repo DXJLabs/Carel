113dd4c5fe0815cec74bdda0d71de75fa2a8cc16
ntfound-dev <ntfound-dev@users.noreply.github.com>
2026-09-19 02:16:57 +0800

    security: harden AVNU execution and unshield privacy

--- lib/carel/networks.ts
+++ lib/carel/networks.ts
@@ -35,6 +35,8 @@ export const CAREL_NETWORKS = {
     rpcUrl: SEPOLIA_RPC,
     provider: sepoliaProvider,
     avnuBaseUrl: SEPOLIA_BASE_URL,
+    avnuExchange:
+      "0x02c56e8b00dbe2a71e57472685378fc8988bba947e9a99b26a00fade2b4fe7c2",
     explorerTx: "https://sepolia.voyager.online/tx/",
     usdcToken:
       "0x0512feac6339ff7889822cb5aa2a86c848e9d392bb0e3e237c008674feed8343",
@@ -50,6 +52,8 @@ export const CAREL_NETWORKS = {
     rpcUrl: MAINNET_RPC,
     provider: mainnetProvider,
     avnuBaseUrl: BASE_URL,
+    avnuExchange:
+      "0x04270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f",
     explorerTx: "https://voyager.online/tx/",
     usdcToken:
       "0x033068f6539f8e6e6b131e6b2b814e6c34a5224bc66947c47dab9dfee93b35fb",
